d636f872bb8e907bd12f3595a0b5eb6f8aa1f467
zhaochuanzhen <zhaochuanzhen@hxrainbow.com>
2019-04-28 15:25:10 +0800

    初始化项目

---
+++ server/src/main/java/com/tripper/oauth/server/platform/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.tripper.oauth.server.platform.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : zhaochuanzhen
+ * @date : 2019/4/28 15:07
+ * @Desc:
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable String id) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "product id : " + id;
+    }
+
+    @GetMapping("/order/{id}")
+    public String getOrder(@PathVariable String id) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "order id : " + id;
+    }
+}
